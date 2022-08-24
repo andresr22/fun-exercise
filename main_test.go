f739dd534a0ae3d35422ea62c104d759557f3be0
Andres Rios <andresrios@lyft.com>
2022-08-24 12:39:55 -0500

    Add actions tests

--- main_test.go
+++ main_test.go
@@ -123,7 +123,7 @@ func TestActions(t *testing.T) {
 	// need to check this test
 	_, err = s.getValues("anotherkey")
 	if err.Error() != "anotherkey does not exist" {
-		t.Errorf("[getValues 'anotherkey' shouldn't exists'] Expected 'anotherkey does not exist', but got '%v'", err)
+		t.Errorf("[getValues 'anotherkey shouldn't exists'] Expected 'anotherkey does not exist', but got '%v'", err)
 	}
 
 	str, err_str := s.getValues("key_str")
@@ -134,5 +134,50 @@ func TestActions(t *testing.T) {
 		t.Errorf("[getValues 'key_str: str'] Expected nil, but got '%v'", err_str)
 	}
 	/*** getValues Tests ***/
+
+	/*** increments Tests ***/
+	v := s.increments("key")
+	if v != "1" {
+		t.Errorf("[increments 'key'] Expected 1, but got '%v'", v)
+	}
+
+	v = s.increments("key_str")
+	if v != "value of key_str is string [str]" {
+		t.Errorf("[increments 'key_str'] Expected 'value of key_str is string [str]', but got '%v'", v)
+	}
+	/*** increments Tests ***/
+
+	/*** decrements Tests ***/
+	v = s.decrements("key")
+	if v != "0" {
+		t.Errorf("[decrements 'key'] Expected 0, but got '%v'", v)
+	}
+
+	v = s.decrements("key_str")
+	if v != "value of key_str is string [str]" {
+		t.Errorf("[increments 'key_str'] Expected 'value of key_str is string [str]', but got '%v'", v)
+	}
+	/*** decrements Tests ***/
+
+	/*** sets Tests ***/
+	if s.sets("new_key", "1234"); s.storeInt["new_key"] != 1234 {
+		t.Errorf("[sets 'new_key: 1234'] Expected '1234', but got '%v'", s.storeInt["new_key"])
+	}
+
+	if s.sets("new_key_str", "test"); s.storeString["new_key_str"] != "test" {
+		t.Errorf("[sets 'new_key_str: test'] Expected 'test', but got '%v'", s.storeString["new_key_str"])
+	}
+	/*** sets Tests ***/
+
+	/*** delete Tests ***/
+	if s.delete("new_key"); stringInMap(s.storeInt, "new_key") {
+		t.Errorf("[delete 'new_key'] Expected 'false', but got '%v', %v", stringInMap(s.storeInt, "new_key"), s.storeInt)
+	}
+
+	s.delete("new_key_str")
+	if _, ok := s.storeString["new_key_str"]; ok {
+		t.Errorf("[delete 'new_key_str'] Expected 'false', but got '%v', %v", ok, s.storeString)
+	}
+	/*** delete Tests ***/
 
 }
