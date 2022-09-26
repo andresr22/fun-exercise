b50d46de925fd733b85a1accc8dddb4e43e237db
Andres Rios <andresrios@lyft.com>
2022-09-26 13:29:16 -0500

    Refactor test

--- main_test.go
+++ main_test.go
@@ -1,183 +1,121 @@
 package main
 
 import (
-	"reflect"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestUtils(t *testing.T) {
-
-	/*** stringInMap Tests ***/
-	m := map[string]int{
+func TestKeyExists(t *testing.T) {
+	m := map[string]interface{}{
 		"test1": 1,
-		"test2": 2,
 	}
 
-	// The key 'test' shouldn't exists
-	if stringInMap(m, "test") {
-		t.Errorf("[stringInMap shouldn't exists] Expected false, but got %v", stringInMap(m, "test"))
+	tests := []struct {
+		key    string
+		exists bool
+	}{
+		{"test", false},
+		{"test1", true},
 	}
 
-	// The key 'test1' should exists
-	if !stringInMap(m, "test1") {
-		t.Errorf("[stringInMap should exists] Expected true, but got %v", stringInMap(m, "test1"))
+	for i, tt := range tests {
+		if tt.exists {
+			assert.True(t, KeyExists(m, tt.key), "test[%d]: %s", i, tt.key)
+		} else {
+			assert.False(t, KeyExists(m, tt.key), "test[%d]: %s", i, tt.key)
+		}
 	}
-	/*** stringInMap Tests ***/
-
-	/*** copyMaps Tests ***/
-	s := stores{
-		storeInt:    make(map[string]int),
-		storeString: make(map[string]string),
-	}
-
-	transactions := transaction{
-		begin: false,
-		stores: stores{
-			storeInt: map[string]int{
-				"test": 1,
-			},
-			storeString: map[string]string{
-				"test": "test",
-			},
-		},
-	}
-	transactionPointer := &transactions
-
-	transactionPointer.copyMaps(s)
-
-	if !reflect.DeepEqual(transactions.stores, s) {
-		t.Errorf("[copyMaps] Expected two equal maps, but got map1 %v != map2 %v", transactions, s)
-	}
-	/*** copyMaps Tests ***/
-
-	/*** validateInput Tests ***/
-	err := validateInput(strings.Split("INCR key", " "))
-	if err != nil {
-		t.Errorf("[validateInput 'INCR key'] Expected nil, but got '%v'", err)
-	}
-
-	err = validateInput(strings.Split("DECR key", " "))
-	if err != nil {
-		t.Errorf("[validateInput 'DECR key'] Expected nil, but got '%v'", err)
-	}
-
-	err = validateInput(strings.Split("GET key", " "))
-	if err != nil {
-		t.Errorf("[validateInput 'GET key'] Expected nil, but got '%v'", err)
-	}
-
-	err = validateInput(strings.Split("SET key value", " "))
-	if err != nil {
-		t.Errorf("[validateInput 'SET key value'] Expected nil, but got '%v'", err)
-	}
-
-	err = validateInput(strings.Split("DELETE key", " "))
-	if err != nil {
-		t.Errorf("[validateInput 'DELETE key'] Expected nil, but got '%v'", err)
-	}
-
-	err = validateInput(strings.Split("INCR key value", " "))
-	if err.Error() != "too many arguments" {
-		t.Errorf("[validateInput 'INCR key value'] Expected 'too many arguments', but got '%v'", err)
-	}
-
-	err = validateInput(strings.Split("INCR", " "))
-	if err.Error() != "missing arguments" {
-		t.Errorf("[validateInput 'INCR'] Expected 'missing arguments', but got '%v'", err)
-	}
-
-	err = validateInput(strings.Split("SET key", " "))
-	if err.Error() != "missing arguments" {
-		t.Errorf("[validateInput 'SET key'] Expected 'missing arguments', but got '%v'", err)
-	}
-
-	err = validateInput(strings.Split("QWERTY", " "))
-	if err.Error() != "invalid action" {
-		t.Errorf("[validateInput 'QWERTY'] Expected 'invalid action', but got '%v'", err)
-	}
-	/*** validateInput Tests ***/
-
 }
 
-func TestActions(t *testing.T) {
+func TestValidateInput(t *testing.T) {
+	tests := []struct {
+		input     string
+		hasError  bool
+		errorKind error
+	}{
+		{"INCR key", false, nil},
+		{"INCR key value", true, TooManyArguments},
+		{"DEC key", true, InvalidAction},
+		{"SET key", true, MissingArguments},
+	}
 
-	s := stores{
-		storeInt: map[string]int{
-			"key": 0,
+	for i, tt := range tests {
+		err := ValidateInput(strings.Split(tt.input, " "))
+		if tt.hasError {
+			assert.Error(t, err, "test[%d]: %s", i, tt.input)
+		} else {
+			assert.NoError(t, err, "test[%d]: %s", i, tt.input)
+			assert.IsType(t, tt.errorKind, err)
+		}
+	}
+}
+
+func TestCopyMap(t *testing.T) {
+
+	tests := []struct {
+		s        StorageInterface
+		fromelem bool
+	}{
+		{
+			StorageInterface{
+				elem: map[string]interface{}{
+					"test1": 1,
+					"test2": "test",
+				},
+				transaction: TransactionInterface{
+					begin: false,
+					elem:  make(map[string]interface{}),
+				},
+			},
+			true,
 		},
-		storeString: map[string]string{
-			"key_str": "str",
+
+		{
+			StorageInterface{
+				elem: make(map[string]interface{}),
+				transaction: TransactionInterface{
+					begin: false,
+					elem: map[string]interface{}{
+						"test1": 1,
+						"test2": "test",
+					},
+				},
+			},
+			false,
 		},
 	}
 
-	/*** getValues Tests ***/
-	n, err := s.getValues("key")
-	if n != "0" {
-		t.Errorf("[getValues 'key: 0'] Expected '0', but got '%v'", n)
+	for i, tt := range tests {
+		tt.s.CopyMap(tt.fromelem)
+		assert.Equal(t, tt.s.elem, tt.s.transaction.elem, "test[%d]: elem(%s) -> transaction.elem(%s)", i, tt.s.elem, tt.s.transaction.elem)
 	}
-	if err != nil {
-		t.Errorf("[getValues 'key: 0'] Expected nil, but got '%v'", err)
+}
+
+func TestSet(t *testing.T) {
+	s := StorageInterface{
+		elem: make(map[string]interface{}),
+		transaction: TransactionInterface{
+			begin: false,
+			elem:  make(map[string]interface{}),
+		},
+	}
+	tests := []struct {
+		key   string
+		value string
+	}{
+		{"string", "value"},
+		{"int", "1"},
 	}
 
-	// need to check this test
-	_, err = s.getValues("anotherkey")
-	if err.Error() != "anotherkey does not exist" {
-		t.Errorf("[getValues 'anotherkey shouldn't exists'] Expected 'anotherkey does not exist', but got '%v'", err)
+	for i, tt := range tests {
+		s.Set(tt.key, tt.value)
+		switch s.elem[tt.key].(type) {
+		case string:
+			assert.Equal(t, "value", s.elem[tt.key], "test[%d]: {%s: %s}", i, tt.key, tt.value)
+		case int:
+			assert.Equal(t, 1, s.elem[tt.key], "test[%d]: {%s: %s}", i, tt.key, tt.value)
+		}
 	}
-
-	str, err_str := s.getValues("key_str")
-	if str != "str" {
-		t.Errorf("[getValues 'key_str: str'] Expected 'str', but got '%v'", str)
-	}
-	if err_str != nil {
-		t.Errorf("[getValues 'key_str: str'] Expected nil, but got '%v'", err_str)
-	}
-	/*** getValues Tests ***/
-
-	/*** increments Tests ***/
-	v := s.increments("key")
-	if v != "1" {
-		t.Errorf("[increments 'key'] Expected 1, but got '%v'", v)
-	}
-
-	v = s.increments("key_str")
-	if v != "value of key_str is string [str]" {
-		t.Errorf("[increments 'key_str'] Expected 'value of key_str is string [str]', but got '%v'", v)
-	}
-	/*** increments Tests ***/
-
-	/*** decrements Tests ***/
-	v = s.decrements("key")
-	if v != "0" {
-		t.Errorf("[decrements 'key'] Expected 0, but got '%v'", v)
-	}
-
-	v = s.decrements("key_str")
-	if v != "value of key_str is string [str]" {
-		t.Errorf("[increments 'key_str'] Expected 'value of key_str is string [str]', but got '%v'", v)
-	}
-	/*** decrements Tests ***/
-
-	/*** sets Tests ***/
-	if s.sets("new_key", "1234"); s.storeInt["new_key"] != 1234 {
-		t.Errorf("[sets 'new_key: 1234'] Expected '1234', but got '%v'", s.storeInt["new_key"])
-	}
-
-	if s.sets("new_key_str", "test"); s.storeString["new_key_str"] != "test" {
-		t.Errorf("[sets 'new_key_str: test'] Expected 'test', but got '%v'", s.storeString["new_key_str"])
-	}
-	/*** sets Tests ***/
-
-	/*** delete Tests ***/
-	if s.delete("new_key"); stringInMap(s.storeInt, "new_key") {
-		t.Errorf("[delete 'new_key'] Expected 'false', but got '%v', %v", stringInMap(s.storeInt, "new_key"), s.storeInt)
-	}
-
-	s.delete("new_key_str")
-	if _, ok := s.storeString["new_key_str"]; ok {
-		t.Errorf("[delete 'new_key_str'] Expected 'false', but got '%v', %v", ok, s.storeString)
-	}
-	/*** delete Tests ***/
-
 }
